59c75f70ecbadd5b67e8cd3b4de32c170f0cac00
C.S NAM(MAC) <csnam515@gmail.com>
2023-01-20 02:54:11 +0900

    Initial Commit

---
+++ NavigationNoStoryBoard-ObjectC/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  NavigationNoStoryBoard-ObjectC
+//
+//  Created by csnam on 2023/01/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ NavigationNoStoryBoard-ObjectC/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  NavigationNoStoryBoard-ObjectC
+//
+//  Created by csnam on 2023/01/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
